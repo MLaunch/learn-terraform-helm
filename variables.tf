0b460a2e24c2ba62a8ca32ce945ba684d9af6441
Matthew <80968027+MLaunch@users.noreply.github.com>
2021-10-20 10:22:19 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,6 +10,5 @@ variable "application_name" {
 variable "slack_app_token" {
   type        = string
   description = "Slack App Token"
-  default = "xoxe.xoxp-1-Mi0yLTI2MjQwMDE5NDk0OTMtMjYxMjMwNjIyMjU1MS0yNjM5NjIyMTIwNTc3LTI2NTA2ODUyMTQ2MDgtYzY5ZGRmOTNhOTk0YmEwNzg1MTJlYmNkODAwMzcyOWIwNTQ4YjM5MjlhZjE4Yzg0ZGI4MDhhN2U0YWYzMGRmOQ"
 }
 
