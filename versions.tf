a874f563c7abf49cfbc1d26d35ea003737b976ef
Matthew <80968027+MLaunch@users.noreply.github.com>
2021-10-22 13:30:42 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,16 +2,16 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.0.1"
+      version = "~> 2.3.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0.1"
+      version = "~> 2.6.1"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "3.53.0"
+      version = "3.63.0"
     }
   }
-  required_version = "~> 1.0.9"
+  required_version = "~> 1.0.8"
 }
